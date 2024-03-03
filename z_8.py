b7c42fa244a84f3937bc41b1d7f3c3c49e8fc5ef
Anonymous59 <illamaslovskij7@gmail.com>
2024-03-03 10:49:48 +0200

    Student

---
+++ z_8.py
@@ -0,0 +1,30 @@
+a = int(input())
+
+if a == 1:
+
+   print("Понеділок")
+
+elif a == 2:
+
+   print("вівторок")
+
+elif a == 3:
+
+   print("середа")
+
+elif a == 4:
+
+   print("четвер")
+
+elif a == 5:
+
+   print("пятниця")
+
+elif a == 6:
+
+   print("субота")
+
+elif a == 7:
+
+   print("неділя")
+
